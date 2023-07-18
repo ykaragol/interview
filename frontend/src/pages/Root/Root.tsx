9bb3e950a5c0b3fe10c2eaaa5b3ca140035c9709
tek-yk <tek-yk@example.com>
2023-07-18 19:31:27 +0300

    added some small notes

--- frontend/src/pages/Root/Root.tsx
+++ frontend/src/pages/Root/Root.tsx
@@ -5,10 +5,10 @@ const Root = () => (
   <div className="flex place-content-center">
     <div className="grow max-w-6xl grid gap-4 grid-cols-1 pt-6">
       <header className="flex items-center">
-        <h1 className="text-3xl grow">Welcome to the GitHub Repo Listing app!</h1>
+        <h1 className="text-3xl font-medium text-slate-700 grow">Welcome to the GitHub Repo Listing app!</h1>
         <Link to="/about" className="text-1xl text-blue-600 visited:text-purple-600 ">About</Link>
       </header>
-      <h4 className="text-md py-2">
+      <h4 className="text-md font-medium text-slate-700 py-2">
         This application is for listing GitHub repositories of users or
         organizations!
       </h4>
